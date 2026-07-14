738891a1e9096513bdf7a5750bcdbd2f280500fb
sgendaneza-web <s.gendaneza@alustudent.com>
2026-07-14 07:00:12 +0000

    commit

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,36 +1,64 @@
 #!/usr/bin/python3
-"""Returns an employee's TODO list progress."""
+
+
+"""
+Retrieve and display the todo list progress for a specific employee.
+
+Args:
+employee_id (int): The ID of the employee for whom to fetch progress.
+
+Returns:
+None
+"""
 
 import requests
 import sys
 
-
 if __name__ == "__main__":
+
+    """
+    Gets the employee ID
+
+    Puts the links of APIs into variables
+
+    Retrieves data from the API and parses the data
+
+    Uses the data to generate a progress report for each employee
+    """
+
+    # Check for the correct number of arguments
+    if len(sys.argv) != 2:
+        print("Usage: ./0-gather_data_from_an_API.py <employee_id>")
+        sys.exit(1)
+
+    # Get the employee ID from the command line argument
     employee_id = sys.argv[1]
 
-    user_url = (
-        "https://jsonplaceholder.typicode.com/users/{}"
-        .format(employee_id)
-    )
-    todos_url = (
-        "https://jsonplaceholder.typicode.com/todos?userId={}"
-        .format(employee_id)
-    )
+    # Define the API endpoints for todos and users
+    todo_url = 'https://jsonplaceholder.typicode.com/todos'
+    user_url = f'https://jsonplaceholder.typicode.com/users/{employee_id}'
 
-    user_info = requests.get(user_url).json()
-    todos_info = requests.get(todos_url).json()
+    # Send HTTP GET requests to retrieve data from the API
+    todo_response = requests.get(todo_url)
+    user_response = requests.get(user_url)
 
-    employee_name = user_info["name"]
+    # Parse the JSON responses into Python dictionaries
+    todo_data = todo_response.json()
+    user_data = user_response.json()
 
-    completed_tasks = [task for task in todos_info if task["completed"]]
+    if 'name' in user_data:
+        name = user_data['name']
 
-    print(
-        "Employee {} is done with tasks({}/{}):".format(
-            employee_name,
-            len(completed_tasks),
-            len(todos_info)
-        )
-    )
+    # Filter completed and total tasks for the specified employee
+    completed_tasks = [task for task in todo_data
+                       if task['userId'] == int(employee_id) and
+                       task['completed']]
+    total_tasks = [task for task in todo_data
+                   if task['userId'] == int(employee_id)]
 
+    # Display the employee's todo list progress
+    fin = len(completed_tasks)
+    sum = len(total_tasks)
+    print(f"Employee {name} is done with tasks({fin}/{sum}):")
     for task in completed_tasks:
-        print("\t {}".format(task["title"]))
+        print("\t " + task['title'])
