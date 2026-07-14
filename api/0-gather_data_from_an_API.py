b7683bca69f68447178ecfdcf7d059d5a6a5bc1a
sgendaneza-web <s.gendaneza@alustudent.com>
2026-07-14 06:55:39 +0000

    commit

--- api/0-gather_data_from_an_API.py
+++ api/0-gather_data_from_an_API.py
@@ -1,29 +1,36 @@
 #!/usr/bin/python3
-"""Module"""
+"""Returns an employee's TODO list progress."""
 
 import requests
 import sys
 
-"""Module"""
 
-if __name__ == '__main__':
-    """IF SCRIPT IS NOT RUN AS MODULE"""
+if __name__ == "__main__":
     employee_id = sys.argv[1]
-    user_url = "https://jsonplaceholder.typicode.com/users/{}" \
+
+    user_url = (
+        "https://jsonplaceholder.typicode.com/users/{}"
         .format(employee_id)
-    todos_url = "https://jsonplaceholder.typicode.com/users/{}/todos/" \
+    )
+    todos_url = (
+        "https://jsonplaceholder.typicode.com/todos?userId={}"
         .format(employee_id)
+    )
 
     user_info = requests.get(user_url).json()
     todos_info = requests.get(todos_url).json()
 
     employee_name = user_info["name"]
-    task_completed = list(filter(lambda obj:
-                                 (obj["completed"] is True), todos_info))
-    number_of_done_tasks = len(task_completed)
-    total_number_of_tasks = len(todos_info)
 
-    print("Employee {} is done with tasks({}/{}):".
-          format(employee_name, number_of_done_tasks, total_number_of_tasks))
+    completed_tasks = [task for task in todos_info if task["completed"]]
 
-    [print("\t " + task["title"]) for task in task_completed]
+    print(
+        "Employee {} is done with tasks({}/{}):".format(
+            employee_name,
+            len(completed_tasks),
+            len(todos_info)
+        )
+    )
+
+    for task in completed_tasks:
+        print("\t {}".format(task["title"]))
